805e7d2f49945c81ee22eec10d39e983a0ceb50b
Mako-Burn-E <jarrodmiller77@gmail.com>
2025-11-07 16:23:46 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Minimal hookup for buy links
-const RAYDIUM = "https://raydium.io/swap/?outputCurrency=BCWX_ADDRESS"; // <-- replace token address
-["buyTop","buyHero","buy1"].forEach(id=>{
-  const el = document.getElementById(id);
-  if (el) el.href = RAYDIUM;
-});
-document.getElementById("dexLink")?.setAttribute("href", RAYDIUM);
